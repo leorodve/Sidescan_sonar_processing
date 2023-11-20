44470bfe8e6b980a3e760122ba798b6857bbdbed
Leonardo Rodriguez <63863187+leorodve@users.noreply.github.com>
2023-11-20 13:41:15 -0400

    Update mosaic_to_geotiff.py

--- mosaic_to_geotiff.py
+++ mosaic_to_geotiff.py
@@ -4,8 +4,9 @@ from bisect import bisect_left
 from rasterio.transform import Affine
 import rasterio
 from scipy.interpolate import interp1d
-import cv2
 import SSS_functions as sss
+from tqdm import tqdm
+import os
 
 def take_closest(myList, myNumber, length):
     """
@@ -41,24 +42,19 @@ def right_pos(myList, pos):
     else:
         return pos-1
 
-
-def grid_data(X_smooth_utm, Y_smooth_utm, X_utm, Y_utm, data, no_pings, resampling):
-    res_x = 0.2
-    res_y = 0.2
-    X = np.arange(np.min(X_smooth_utm)-100, np.max(X_smooth_utm)+100, res_x)
-    Y = np.arange(np.min(Y_smooth_utm)-100, np.max(Y_smooth_utm)+100, res_y)
+def grid_data(X_smooth_utm, Y_smooth_utm, X_utm, Y_utm, data, no_pings, resampling, resolution):
+    X = np.arange(np.min(X_smooth_utm)-100, np.max(X_smooth_utm)+100, resolution)
+    Y = np.arange(np.min(Y_smooth_utm)-100, np.max(Y_smooth_utm)+100, resolution)
     mosaic = np.zeros((len(Y),len(X)))
 
     # position the data in the correct coordinate of the new grid
-    for a in range(0,no_pings):
-        print(a)
+    print("Gridding data")
+    for a in tqdm(range(no_pings)):
         id_x = take_closest(X, X_utm[a,:,0], resampling)
         id_y = len(Y) - take_closest(Y, Y_utm[a,:,0], resampling)
-        mosaic[id_y, id_x] = data[:,a,0]
-    
-    # interpolate values in empty pixels
-    for b in range(0, mosaic.shape[1]):
-        print(b)
+        mosaic[id_y, id_x] = data[:,a]
+        
+    for b in tqdm(range(mosaic.shape[1])):
         non_zeros = np.argwhere(mosaic[:,b] != 0)
         try:
             first = non_zeros[0][0]
@@ -68,25 +64,43 @@ def grid_data(X_smooth_utm, Y_smooth_utm, X_utm, Y_utm, data, no_pings, resampli
                               len(mosaic[non_zeros, b])))(np.arange(first, last))
         except:
             IndexError
+    mosaic = np.nan_to_num(mosaic)
     return mosaic
 
-def save_mosaic(mosaic, x_utm, y_utm):
-    #save it first as png to create the rgb matrix
-    plt.imsave('mosaic_png.png', mosaic, cmap=sss.Color_palette())
-    
-    img = cv2.imread('mosaic_png.png')  #cv2 read the images as bgr
-    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def scalar_to_rgb(data):
+    #Transform scalar-data to four dimensional RGBA data; the fourth is alpha
+    #First we must normalize the data
+    norm = plt.Normalize(vmin=data.min(), vmax=data.max())
+    #Map the scalar-data using colormap function from matplotlib
+    #The default cmap used by matplotlib is 'viridis', but that can be changed
+    #using different cmaps in the function below
+    # cm = plt.cm.viridis
+    #or a cmap defined by the user
+    cm = sss.Color_palette()
+    #cm = plt.cm.gist_rainbow
+    #cm = plt.cm.viridis
 
-    res_x = 0.2
-    res_y = 0.2
-    X = np.arange(np.min(x_utm)-100, np.max(x_utm)+100, res_x)
-    Y = np.arange(np.min(y_utm)-100, np.max(y_utm)+100, res_y)
-    transform = Affine.translation(np.min(X) - res_x / 2, np.min(Y) - res_y / 2) * Affine.scale(res_x, res_y)
+    rgb_data = cm(norm(data)) #four dimensional matrix
+    """
+    It is important to note that once the data is saved using a certain cmap,
+    it CAN'T be changed when plotting. 
+    """
+    #Now we re-escale the data from 0.0-1.0 float scale, to 0-255 integer scale
+    rgb_data = (rgb_data[:,:,:3]*255).astype('uint8')
+    # rgb_data = rgb_dat
+
+def save_mosaic(mosaic, x_utm, y_utm, resolution, filepath):
+    img_rgb = scalar_to_rgb(mosaic)
+    X = np.arange(np.min(x_utm)-100, np.max(x_utm)+100, resolution)
+    Y = np.arange(np.min(y_utm)-100, np.max(y_utm)+100, resolution)
+    transform = Affine.translation(np.min(X) - resolution / 2, np.min(Y) - resolution / 2) * Affine.scale(resolution, resolution)
     #reshape img to save as geotiff
     #goes from [height,width,bands] to [bands, height, width]
     image = np.moveaxis(img_rgb.squeeze(),-1,0)
+    file_name_with_ext = os.path.basename(filepath[0])
+    file_name = os.path.splitext(file_name_with_ext)[0]
     new_dataset = rasterio.open(
-        'new_mosaic.tif',
+        '%s.tif' %file_name,
         'w',
         driver='GTiff',
         height=mosaic.shape[0],
