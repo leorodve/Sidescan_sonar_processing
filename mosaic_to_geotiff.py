dddfe5e5228794733bd38132b60dc47364603c20
Leonardo Rodriguez <63863187+leorodve@users.noreply.github.com>
2025-07-01 10:12:24 -0400

    Update mosaic_to_geotiff.py

--- mosaic_to_geotiff.py
+++ mosaic_to_geotiff.py
@@ -14,9 +14,9 @@ def take_closest(myList, myNumber, length):
 
     If two numbers are equally close, return the smallest number.
     """
-    pos = [bisect_left(myList, myNumber[i]) for i in range(0,2*length)]
+    pos = [bisect_left(myList, myNumber[i]) for i in range(2*length)]
     position = np.zeros_like(pos)
-    for i in range(0,2*length):
+    for i in range(2*length):
         if pos[i] == 0:
             position[i] = 0
         if pos[i] == len(myList):
@@ -42,18 +42,20 @@ def right_pos(myList, pos):
     else:
         return pos-1
 
-def grid_data(X_smooth_utm, Y_smooth_utm, X_utm, Y_utm, data, no_pings, resampling, resolution):
-    X = np.arange(np.min(X_smooth_utm)-100, np.max(X_smooth_utm)+100, resolution)
-    Y = np.arange(np.min(Y_smooth_utm)-100, np.max(Y_smooth_utm)+100, resolution)
+
+def grid_data(X, Y, X_utm, Y_utm, data, no_pings, resampling, resolution, file_number, file):
     mosaic = np.zeros((len(Y),len(X)))
 
     # position the data in the correct coordinate of the new grid
-    print("Gridding data")
+    file_name_with_ext = os.path.basename(file[file_number])
+    file_name = os.path.splitext(file_name_with_ext)[0]
+    print("Gridding data %s" %file_name)
     for a in tqdm(range(no_pings)):
-        id_x = take_closest(X, X_utm[a,:,0], resampling)
-        id_y = len(Y) - take_closest(Y, Y_utm[a,:,0], resampling)
-        mosaic[id_y, id_x] = data[:,a]
+        id_x = take_closest(X, X_utm[a,:,file_number], resampling)
+        id_y = len(Y) - take_closest(Y, Y_utm[a,:,file_number], resampling)
+        mosaic[id_y, id_x] = data[:,a,file_number]
         
+    # interpolate values in empty pixels
     for b in tqdm(range(mosaic.shape[1])):
         non_zeros = np.argwhere(mosaic[:,b] != 0)
         try:
@@ -67,18 +69,17 @@ def grid_data(X_smooth_utm, Y_smooth_utm, X_utm, Y_utm, data, no_pings, resampli
     mosaic = np.nan_to_num(mosaic)
     return mosaic
 
-def scalar_to_rgb(data):
+def scalar_to_rgb(data, color_min, color_max):
     #Transform scalar-data to four dimensional RGBA data; the fourth is alpha
     #First we must normalize the data
-    norm = plt.Normalize(vmin=data.min(), vmax=data.max())
+    norm = plt.Normalize(vmin=color_min, vmax=color_max)
     #Map the scalar-data using colormap function from matplotlib
     #The default cmap used by matplotlib is 'viridis', but that can be changed
     #using different cmaps in the function below
     # cm = plt.cm.viridis
     #or a cmap defined by the user
     cm = sss.Color_palette()
-    #cm = plt.cm.gist_rainbow
-    #cm = plt.cm.viridis
+    # cm = plt.cm.gist_rainbow
 
     rgb_data = cm(norm(data)) #four dimensional matrix
     """
@@ -87,10 +88,12 @@ def scalar_to_rgb(data):
     """
     #Now we re-escale the data from 0.0-1.0 float scale, to 0-255 integer scale
     rgb_data = (rgb_data[:,:,:3]*255).astype('uint8')
-    # rgb_data = rgb_dat
+    return rgb_data
 
-def save_mosaic(mosaic, x_utm, y_utm, resolution, filepath):
-    img_rgb = scalar_to_rgb(mosaic)
+def save_mosaic(mosaic, x_utm, y_utm, resolution, filepath, color_min, color_max):
+    #save it first as png to create the rgb matrix
+    img_rgb = scalar_to_rgb(mosaic, color_min, color_max)
+
     X = np.arange(np.min(x_utm)-100, np.max(x_utm)+100, resolution)
     Y = np.arange(np.min(y_utm)-100, np.max(y_utm)+100, resolution)
     transform = Affine.translation(np.min(X) - resolution / 2, np.min(Y) - resolution / 2) * Affine.scale(resolution, resolution)
