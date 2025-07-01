55797424fa0aa5669074933ca52fc948bb8b28fd
Leonardo Rodriguez <63863187+leorodve@users.noreply.github.com>
2025-07-01 10:27:48 -0400

    Update main_file.py

--- main_file.py
+++ main_file.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import os
 import matplotlib.pyplot as plt
@@ -13,10 +14,10 @@ import mosaic_to_geotiff as ma
 # Function to upload files selected by the user
 def upload():
     global filepath
-    filepath = filedialog.askopenfilenames(initialdir="/", title="Select A XTF File", filetypes=(("XTF files", "*.xtf"),("all files", "*.*")))
+    filepath = tkinter.filedialog.askopenfilenames(initialdir="/", title="Select A XTF File", filetypes=(("XTF files", "*.xtf"),("all files", "*.*")))
 
 def welcome_window():
-    #labels and grid
+        #labels and grid
     ttk.Button(root, text="Please select a XTF file", command=upload).grid(column=1, row=1)
     ttk.Label(root, text="Select a XTF file").grid(column=0, row=1)
     #Resampling entries
@@ -46,14 +47,23 @@ def welcome_window():
     ttk.Button(root, text="Next", command=display_data_window).grid(column=0, row=9, columnspan=4)
     return root
 
-# Plot survey lines in mosaic
-def plot_data(window):
+# Plot the lines in mosaic
+def plot_data(window, Xutm, Yutm, histogram):
     my_colors = sss.Color_palette()
     figure = Figure(figsize=(10, 6), dpi=100)
 
-    
     ax1 = figure.subplots(1, 1)
-    ax1.imshow(high_res_grid, cmap=my_colors, aspect='auto')
+    if histogram == 0:
+        minimum = 0
+        maximum = 2**16/2        
+    if histogram == 1:
+        minimum = np.percentile((port_chan, stbd_chan), 0)
+        maximum = np.percentile((port_chan, stbd_chan), 100)
+    if histogram == 2:
+        minimum = np.percentile((port_chan, stbd_chan), 1)
+        maximum = np.percentile((port_chan, stbd_chan), 100)
+    color_values = np.arange(minimum, maximum)    
+    ax1.imshow(high_res_grid, cmap=my_colors, aspect='auto', vmin=np.percentile(color_values, 0), vmax=np.percentile(color_values, 100))
 
     ax1.set_xlabel('X distance [m]')
     ticks_x = np.arange(0, high_res_grid.shape[1], int(high_res_grid.shape[1]/5))
@@ -69,6 +79,7 @@ def plot_data(window):
         ticks_labels_y = np.append(ticks_labels_y, np.round(max(Yutm),2))
     ax1.set_yticks(ticks_y)
     ax1.set_yticklabels(ticks_labels_y)
+
     
     canvas = FigureCanvasTkAgg(figure, window)
     return canvas
@@ -77,22 +88,22 @@ def plot_data(window):
 def plot_waterfall(window, port_data, stbd_data, settings):
 
     my_colors = sss.Color_palette()
-    color_values = np.arange(0, 2**16/2, 2**16/2/255)
-    #color_values = np.arange(255)
+    minimum = np.percentile((port_data, stbd_data), 1)
+    maximum = np.percentile((port_data, stbd_data), 100)
+    color_values = np.arange(minimum, maximum)
     file_name_with_ext = os.path.basename(filepath[file_number])
     file_name = os.path.splitext(file_name_with_ext)[0]
     figure = plt.figure(figsize=(12, 7), dpi=100)
     gs = gridspec.GridSpec(1, 2, wspace=0.0)
 
-    # figure.suptitle('Raw data \n File name: %s' %file_name)
     ax1 = figure.add_subplot(gs[0, 0])
     ax2 = figure.add_subplot(gs[0,1])
     
     if settings[7] == "AGC":
         print("Calculating AGC")
         data = np.asarray([sss.Automatic_Gain_Control(port_data[:,i,settings[10]], stbd_data[:,i,settings[10]], settings[8], settings[9]) for i in tqdm(range(int(pings_per_file[settings[10]+1,0])))]).T
-        port_data = data[:1024, :]
-        stbd_data = data[1024:, :]
+        port_data = data[:int(resampling/2), :]
+        stbd_data = data[int(resampling/2):, :]
         
         pos = ax1.imshow(port_data[:,settings[4]:settings[5]].T, cmap=my_colors, aspect='auto', vmin=np.percentile(color_values, settings[2]), vmax=np.percentile(color_values, settings[3]))
         pos1 = ax2.imshow(stbd_data[:, settings[4]:settings[5]].T, cmap=my_colors, aspect='auto', vmin=np.percentile(color_values, settings[0]), vmax= np.percentile(color_values, settings[1]))
@@ -100,8 +111,8 @@ def plot_waterfall(window, port_data, stbd_data, settings):
     if settings[7] == "MedFilt":
         print("Calculating MedFilt")
         data = np.asarray([sss.Median_filter(port_data[:,i,settings[10]], stbd_data[:,i,settings[10]], settings[8]) for i in tqdm(range(int(pings_per_file[settings[10]+1,0])))]).T
-        port_data = data[:1024, :]
-        stbd_data = data[1024:, :]
+        port_data = data[:int(resampling/2), :]
+        stbd_data = data[int(resampling/2):, :]
         
         pos = ax1.imshow(port_data[:,settings[4]:settings[5]].T, cmap=my_colors, aspect='auto', vmin=np.percentile(color_values, settings[2]), vmax=np.percentile(color_values, settings[3]))
         pos1 = ax2.imshow(stbd_data[:, settings[4]:settings[5]].T, cmap=my_colors, aspect='auto', vmin=np.percentile(color_values, settings[0]), vmax= np.percentile(color_values, settings[1]))
@@ -111,10 +122,10 @@ def plot_waterfall(window, port_data, stbd_data, settings):
         pos1 = ax2.imshow(stbd_data[:, settings[4]:settings[5],settings[10]].T, cmap=my_colors, aspect='auto', vmin=np.percentile(color_values, settings[0]), vmax= np.percentile(color_values, settings[1]))
 
     figure.colorbar(pos, ax=[ax1], location='left', pad=0.2)
-    # ax1.set_xlabel('Port-side distance [m]')
-    ticks_x = np.arange(0, resampling+1, 256)
-    increase = 75/4
-    ticks_labels_x = np.arange(0, 75+1, increase)
+    ticks_x = np.arange(0, resampling+1, int(resampling/4))
+    max_hor_range = sss.Get_max_range(filepath, file_number)
+    increase = max_hor_range/4
+    ticks_labels_x = np.arange(0, max_hor_range+1, increase)
     ticks_labels_x1 = np.flip(ticks_labels_x)
     ax1.set_xticks(ticks_x)
     ax1.set_xticklabels(ticks_labels_x1)
@@ -127,10 +138,7 @@ def plot_waterfall(window, port_data, stbd_data, settings):
     ax1.set_yticklabels(ticks_labels_y)
     
     ax2.yaxis.tick_right()
-    # pos1 = ax2.imshow(stbd_data[:, settings[4]:settings[5],0].T, cmap=my_colors, aspect='auto', vmin=np.percentile(stbd_data[:,:,0], settings[0]), vmax= np.percentile(stbd_data[:,:,0], settings[1]))
     figure.colorbar(pos1, ax=[ax2], location='right', pad=0.2)
-    
-    # ax2.set_xlabel('Starboard-side distance [m]')
     
     ax2.set_xticks(ticks_x)
     ax2.set_xticklabels(ticks_labels_x)
@@ -176,29 +184,25 @@ def labels():
     ttk.Label(root, text="Last ping").grid(column=5, row=11, columnspan=2)
     ping_second_entry = ttk.Entry(root, textvariable=last_ping_entry)
     ping_second_entry.grid(column=5, row=12, columnspan=2)
+    
 
-#Function calling the widget to plot the data in waterfall view
 def waterfall_window():
     for widget in root.winfo_children():
         widget.destroy()
-        stbd_min_ent = stbd_min_entry.get() 
+    stbd_min_ent = stbd_min_entry.get() 
     stbd_max_ent = stbd_max_entry.get()
     port_max_ent = port_max_entry.get()
     port_min_ent = port_min_entry.get()
     first_ping_ent = first_ping_entry.get()
     last_ping_ent = last_ping_entry.get()
-    mosaic_resolution = float(resolution_entry.get())
-    ma.save_mosaic(high_res_grid, Xsmooth_utm, Ysmooth_utm, mosaic_resolution, filepath)
     display_settings = [stbd_min_ent, stbd_max_ent, port_min_ent, port_max_ent,
                         first_ping_ent, last_ping_ent, 'no', None, 0, 0, file_number]
     waterfall_plot = plot_waterfall(root, port_chan, stbd_chan, display_settings)
-    waterfall_plot.get_tk_widget().grid(row=0, column=0, columnspan=4, rowspan=16)
+    waterfall_plot.get_tk_widget().grid(row=0, column=0, columnspan=4, rowspan=15)
     labels()
     ttk.Button(root, text="Slant-corrected", command=correct_slant).grid(column=5, row=13, columnspan=2)
     ttk.Button(root, text="Plot next file", command=display_next_file).grid(column=5, row=14, columnspan=2)
-    ttk.Button(root, text="Save mosaic as geotiff", command=save_geotiff).grid(column=5, row=15, columnspan=2)
 
-#Apply slant correction to the data
 def correct_slant():
     for widget in root.winfo_children():
         widget.destroy()
@@ -231,7 +235,6 @@ def correct_slant():
 
     ttk.Button(root, text="Plot next file", command=display_next_file).grid(column=5, row=21, columnspan=2)
 
-#Display the data using the AGC parameters defined by the user
 def display_AGC():
     for widget in root.winfo_children():
         widget.destroy()
@@ -252,7 +255,6 @@ def display_AGC():
     ttk.Button(root, text="Without gain", command=correct_slant).grid(column=5, row=13, columnspan=2)
     ttk.Button(root, text="Plot next file", command=display_next_file).grid(column=5, row=14, columnspan=2)
 
-#Display the data using the MedFilt parameters defined by the user
 def display_MedFilt():
     for widget in root.winfo_children():
         widget.destroy()
@@ -272,7 +274,6 @@ def display_MedFilt():
     ttk.Button(root, text="Without gain", command=correct_slant).grid(column=5, row=13, columnspan=2)
     ttk.Button(root, text="Plot next file", command=display_next_file).grid(column=5, row=14, columnspan=2)
 
-# Display the data using user defined parameters
 def display_again():
     for widget in root.winfo_children():
         widget.destroy()
@@ -305,7 +306,6 @@ def display_again():
 
     ttk.Button(root, text="Plot next file", command=display_next_file).grid(column=5, row=21, columnspan=2)
     
-# Display next file
 def display_next_file():
     global file_number
     for widget in root.winfo_children():
@@ -320,19 +320,53 @@ def display_next_file():
     display_settings = [stbd_min_ent, stbd_max_ent, port_min_ent, port_max_ent,
                         first_ping_ent, last_ping_ent, 'no', None, 0, 0, file_number]
     waterfall_plot = plot_waterfall(root, port_chan, stbd_chan, display_settings)
-    waterfall_plot.get_tk_widget().grid(row=0, column=0, columnspan=4, rowspan=16)
+    waterfall_plot.get_tk_widget().grid(row=0, column=0, columnspan=4, rowspan=15)
     labels()
     ttk.Button(root, text="Slant-corrected", command=correct_slant).grid(column=5, row=13, columnspan=2)
     ttk.Button(root, text="Plot next file", command=display_next_file).grid(column=5, row=14, columnspan=2)
-    ttk.Button(root, text="Save mosaic as geotiff", command=save_geotiff).grid(column=5, row=15, columnspan=2)
-
+        
 def save_geotiff():
+    if scale_value == 0:
+        minimum = 0
+        maximum = 2**16/2        
+    if scale_value == 1:
+        minimum = np.percentile((port_chan, stbd_chan), 0)
+        maximum = np.percentile((port_chan, stbd_chan), 100)
+    if scale_value == 2:
+        minimum = np.percentile((port_chan, stbd_chan), 1)
+        maximum = np.percentile((port_chan, stbd_chan), 100) 
     mosaic_resolution = float(resolution_entry.get())
-    ma.save_mosaic(high_res_grid, Xsmooth_utm, Ysmooth_utm, mosaic_resolution)
+    ma.save_mosaic(high_res_grid, Xsmooth_utm, Ysmooth_utm, mosaic_resolution, filepath, minimum, maximum)
+    print("Mosaic saved as geotiff in this folder: %s" %(os.getcwd()))
+    
+def fit_to_data():
+    global scale_value
+    for widget in root.winfo_children():
+        widget.destroy()
+    scale_value = 1
+    first_plot = plot_data(root, Xsmooth_utm, Ysmooth_utm, 1)
+    first_plot.get_tk_widget().grid(row=1, column=0, columnspan=4)
+    
+    ttk.Button(root, text="Waterfall view", command=waterfall_window).grid(column=0, row=0)
+    ttk.Button(root, text="Fit color scale to data", command=fit_to_data).grid(column=1, row=0)
+    ttk.Button(root, text="Fit color scale to best", command=fit_to_best).grid(column=2, row=0)
+    ttk.Button(root, text="Save mosaic as geotiff", command=save_geotiff).grid(column=3, row=0)
+    
+def fit_to_best():
+    global scale_value
+    for widget in root.winfo_children():
+        widget.destroy()
+    first_plot = plot_data(root, Xsmooth_utm, Ysmooth_utm, 2)
+    first_plot.get_tk_widget().grid(row=1, column=0, columnspan=4)
+    scale_value = 2
+    
+    ttk.Button(root, text="Waterfall view", command=waterfall_window).grid(column=0, row=0)
+    ttk.Button(root, text="Fit color scale to data", command=fit_to_data).grid(column=1, row=0)
+    ttk.Button(root, text="Fit color scale to best", command=fit_to_best).grid(column=2, row=0)
+    ttk.Button(root, text="Save mosaic as geotiff", command=save_geotiff).grid(column=3, row=0)
 
-# Main function, for correcting and applying gains to the data, as well as plotting
 def display_data_window():
-    global filepath, np_chan1, port_chan, new_port_chan, resampling, port_chan_agc, port_chan_median, stbd_chan, new_stbd_chan, stbd_chan_agc, stbd_chan_median, utmX, utmY, sonar_data, pings_per_file, Xposition, Yposition, Xsmooth, Ysmooth, xy, utmX1, utmY1, factor_stbd_x1, factor_stbd_y1, factor_port_x1, factor_port_y1, Xsmooth_utm, Ysmooth_utm, lat, long, high_res_grid, file_number
+    global filepath, port_chan, new_port_chan, resampling, stbd_chan, new_stbd_chan, pings_per_file, Xsmooth_utm, Ysmooth_utm, high_res_grid, file_number, scale_value
     # loop for cleaning the GUI window
     for widget in root.winfo_children():
         widget.destroy()
@@ -345,6 +379,7 @@ def display_data_window():
     duration = duration_entry.get()
     mosaic_resolution = float(resolution_entry.get())
     print('Comienzo del prog')
+    
     sonar_altitude, Xposition, Yposition, ship_gyro, sensor_speed, sensor_heading, layback, slant_port, slant_stbd, pings_per_file, port_chan, stbd_chan = sss.load_data(filepath, resampling)
     max_no_pings = int(np.max(pings_per_file))
     
@@ -371,9 +406,7 @@ def display_data_window():
     utmX, utmY, sonar_data = np.zeros((max_no_pings, 2*resampling, len(filepath))), np.zeros((max_no_pings, 2*resampling, len(filepath))), np.zeros((2*resampling, max_no_pings,len(filepath)))
     Xsmooth = np.zeros((max_no_pings, len(filepath)))
     Ysmooth = np.zeros((max_no_pings, len(filepath)))
-    for file_number in range(len(filepath)):
-        print('file number:', file_number)
-        
+    for file_number in range(len(filepath)):        
         if method_nav:
             
             Xsmooth[:int(pings_per_file[file_number+1, 0]), file_number], Ysmooth[:int(pings_per_file[file_number+1, 0]), file_number], factor_stbd_y, factor_stbd_x, factor_port_y, factor_port_x = sss.smooth_data(Xposition, Yposition, max_no_pings, pings_per_file, pings_smooth, file_number, ship_gyro)
@@ -387,7 +420,7 @@ def display_data_window():
             ping_utmX = np.c_[stbd_utmX, port_utmX]
             ping_utmY = np.c_[stbd_utmY, port_utmY]
             
-            #matriz con coordenadas port y starboard de toda la data
+            #matrix with port and starboard coordinates of all data
             utmX[:int(pings_per_file[file_number+1, 0]), :, file_number] = ping_utmX
             utmY[:int(pings_per_file[file_number+1, 0]), :, file_number] = ping_utmY
         
@@ -410,25 +443,36 @@ def display_data_window():
             sensor_heading[heading_index, file_number] = sensor_heading[heading_index, file_number] + 360
             factor_port_y, factor_port_x = sss.Angle(sensor_heading[:int(pings_per_file[file_number+1, 0]), file_number]-90)
             
-            #Port and starboard utm coordinates
+            '''
+            THE NEXT FEW LINES ARE USING THE VALUE 75 FOR THE SLANT RANGE, THIS SHOULD BE USING AN AUTOMATED VALUE
+            '''
+            # Starboard and port utm coordinates
             stbd_utmY = lat[:, None] + np.multiply.outer(factor_stbd_y, abc) * 75 / resampling
             stbd_utmX = long[:, None] + np.multiply.outer(factor_stbd_x, abc) * 75 / resampling
             port_utmY = lat[:, None] + np.multiply.outer(factor_port_y, resampling-1-abc) * 75 / resampling
             port_utmX = long[:, None] + np.multiply.outer(factor_port_x, resampling-1-abc) * 75 / resampling
             
-            #matrix with ping port and starboard coordinates
+            # Matrix with port and starboard coordinates of ping
             ping_utmX = np.c_[stbd_utmX, port_utmX]
             ping_utmY = np.c_[stbd_utmY, port_utmY]
+            
+            # Matrix with port and starboard coordinates of all data
             
             utmX[:int(pings_per_file[file_number+1, 0]), :, file_number] = ping_utmX
             utmY[:int(pings_per_file[file_number+1, 0]), :, file_number] = ping_utmY
 
-        #Matrix with ping port and starboard samples
+            # Coordenadas en latitud/longitud de starboard y port
+            '''
+            stbd_longitude1, stbd_latitude1 = sss.Lat_Long(stbd_utmX, stbd_utmY, True)
+            port_longitude1, port_latitude1 = sss.Lat_Long(port_utmX, port_utmY, True)
+            '''
+
+        #matrix with ping measurements of port and starboard
         ping_sonar_data = np.r_[np.flip(new_stbd_chan[:,:int(pings_per_file[file_number+1, 0]), file_number], axis=0), np.flip(new_port_chan[:,:int(pings_per_file[file_number+1, 0]), file_number], axis=0)]
         
-        #Matrix with port and starboard samples of all data
+        #matrix with all data measurements from port and starboard
         sonar_data[:, :int(pings_per_file[file_number+1, 0]), file_number] = ping_sonar_data
-
+    
     #Creating matrix for mosaic
     utm_non_zeros = np.argwhere(utmX != 0)
     X = np.arange(np.min(utmX[utm_non_zeros[:,0], utm_non_zeros[:,1], utm_non_zeros[:,2]])-100, np.max(utmX)+100, mosaic_resolution)
@@ -438,12 +482,16 @@ def display_data_window():
     for file_number in range(len(filepath)):
         high_res_grid += ma.grid_data(X, Y, utmX, utmY, sonar_data, int(pings_per_file[file_number+1, 0]), resampling, mosaic_resolution, file_number, filepath)
         high_res_grid.clip(0, sonar_data.max(), out=high_res_grid)
-        
-    first_plot = plot_data(root, Xsmooth_utm, Ysmooth_utm)
-    first_plot.get_tk_widget().grid(row=1, column=0)
-
+    
+    first_plot = plot_data(root, Xsmooth_utm, Ysmooth_utm, 0)
+    first_plot.get_tk_widget().grid(row=1, column=0, columnspan=4)
+    
     file_number = 0
+    scale_value = 0
     ttk.Button(root, text="Waterfall view", command=waterfall_window).grid(column=0, row=0)
+    ttk.Button(root, text="Fit color scale to data", command=fit_to_data).grid(column=1, row=0)
+    ttk.Button(root, text="Fit color scale to best", command=fit_to_best).grid(column=2, row=0)
+    ttk.Button(root, text="Save mosaic as geotiff", command=save_geotiff).grid(column=3, row=0)
 
 
 # ----------- Window widget asking the user for file/parameters --------------
